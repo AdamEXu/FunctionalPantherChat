8221913fd58dfcac703200b3d23224ee92b8bcd4
AdamEXu <51470750+AdamEXu@users.noreply.github.com>
2022-08-14 22:40:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ tokens = []
 
 @app.route('/')
 def index():
-    resp = make_response(render_template('index.html'))
-    c.execute('''
-        SELECT user_id FROM users WHERE user_id = ?
-    ''', )
-    resp.set_cookie('token', user)
-    return resp
+    return render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         message = request.form['username']
+
         message_bytes = message.encode('ascii')
         base64_bytes = base64.b64encode(message_bytes)
         base64_message = base64_bytes.decode('ascii')
+        resp.set_cookie('token', user)
+        return resp
+    else:
+        return render_template('login.html')
