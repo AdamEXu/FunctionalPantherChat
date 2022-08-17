1dfe18bf0dedb5d971a958d760f0a1eda350c210
AdamEXu <51470750+AdamEXu@users.noreply.github.com>
2022-08-17 12:09:40 -0700

    Update app.py
    implemented part of login

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import sqlite3
 conn = sqlite3.connect('pantherchat.db') 
 c = conn.cursor()
 
+def sqlexec(query):
+    c.execute(query)
+
 app = Flask(__name__)
 
 configs = []
@@ -20,13 +23,14 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        resp = make_response(render_template("login.html"))
-        message = request.form['username']
-
-        message_bytes = message.encode('ascii')
-        base64_bytes = base64.b64encode(message_bytes)
-        base64_message = base64_bytes.decode('ascii')
-        resp.set_cookie('token', user)
+        username = request.form("email")
+        userid = -1
+        if ('@' in username and '.' in username:
+            userid = c.execute("SELECT user_id FROM users WHERE email = ?", username)
+        elif (checkallowedcharacters(username):
+            print("YAY!")
+        else:
+            print("NAY!")
         return resp
     else:
         return render_template('login.html')
@@ -48,3 +52,5 @@ def register():
 @app.route('/feed', methods=['GET', 'POST'])
 def feed():
     return render_template("feed.html")
+
+connection.close()
