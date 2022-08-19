2dbd8827941d7598291ce5fa487514202a6a28a0
AdamEXu <51470750+AdamEXu@users.noreply.github.com>
2022-08-19 09:49:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 import base64
 import datetime
+import string
 import random
 import os
 
@@ -14,7 +15,7 @@ def sqlexec(query):
 
 app = Flask(__name__)
 
-allowedcharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+allowedcharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 def checkallowedcharacters(username):
     for character in username:
         if not character in allowedcharacters:
@@ -32,16 +33,25 @@ def index():
 def login():
     if request.method == 'POST':
         username = request.form("email")
+        unencpw = request.form("password")
+        random.seed(unencpw)
+        encryptedpw = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=N))
+        random.seed(int(datetime.time.utc_now()))
         userid = -1
         if '@' in username and '.' in username:
             userid = c.execute("SELECT user_id FROM users WHERE email = ?", username)
-        elif checkallowedcharacters(username):
-            token = os.urandom(20)
-            while tokens[token] != 0:
-                
-            return redirect("feed.html")
         else:
-            flash
+            userid = c.execute("SELECT user_id FROM users WHERE username = ?", username)
+        if checkallowedcharacters(username) and userid != -1:
+            # Get password from sql database
+            password = c.execute("SELECT password FROM users WHERE userid = ?", userid)
+            if True:
+                token = os.urandom(20)
+                while tokens[token] != 0:
+                    token = os.urandom(20)
+                return redirect("feed.html")
+        else:
+            flash "Invalid account"
         return render_template('login.html')
     else:
         return render_template('login.html')
